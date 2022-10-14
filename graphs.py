7673e97fae47baab469f5d7be303736e424c043f
juanamador10 <juan.amador.is@unipolidgo.edu.mx>
2022-10-14 14:20:47 -0500

    School Commit 2

---
+++ graphs.py
@@ -0,0 +1,4 @@
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots()
+ax.scatter([1, 2, 3, 4], [1, 2, 0, 0.5])
+plt.show()
--- read_xml.py
+++ read_xml.py
@@ -1,7 +1,16 @@
 import xml.etree.ElementTree as ET
-tree = ET.parse('xml_test.xml')
+tree = ET.parse('xml_csv.xml')
 root = tree.getroot()
 
-for h in root:
-    print(h.tag)
-    
+
+'''for i in range(len(root)):
+    print(root[i][0].text, root[i][1].text, root[i][2].text)'''
+
+vivienda = []
+for i in range(len(root)):
+    print(root[i][0].text)
+
+    if root[i][0] in vivienda:
+        pass
+    else:
+        vivienda.append()
